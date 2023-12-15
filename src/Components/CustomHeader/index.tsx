39c0720d462f171f8449e2af23e21662c9120a64
taikool2121996 <taikool2121996@gmail.com>
2023-12-15 15:09:52 +0700

    remove navbar collapse

--- src/Components/CustomHeader/index.tsx
+++ src/Components/CustomHeader/index.tsx
@@ -1,14 +1,12 @@
 // libs
 import { FC } from 'react';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'next/image';
 // styled-components
 import { CustomHeaderWrapper, LogoHeader } from './CustomHeader.styled';
 // components
 import { logo } from '../../../public/assets';
-import { navLinks } from '../../constants';
 
 const CustomHeader: FC = () => {
   return (
@@ -26,13 +24,6 @@ const CustomHeader: FC = () => {
             </LogoHeader>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="ml-5">
-            {navLinks.map((nav) => (
-              <Nav className="ml-2" key={nav.id}>
-                <Nav.Link href="#home">{nav.title}</Nav.Link>
-              </Nav>
-            ))}
-          </Navbar.Collapse>
         </Container>
       </Navbar>
     </CustomHeaderWrapper>
